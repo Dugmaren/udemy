cde6d3ca591f016e8afae374479a0632415b61a9
Christopher Anderson <31716308+Dugmaren@users.noreply.github.com>
2018-01-22 21:22:10 -0800

    changed message to test git etc and redeploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();  // this is the part that talks to Node, takes a request
                         // and sends the request to the appropriate handler.
 
 app.get('/', (req, res) => {    // this is the route handler for address '/'
-    res.send({it: 'things to be done' });   // req & res are JS objects
+    res.send({it: 'things to do' });   // req & res are JS objects
 });
 
 const PORT = process.env.PORT || 5000;  // Heroku should define the port at runtime
