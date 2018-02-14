30512c1597e4bd31b9b24d98acf8bb68388c6da1
Christopher Anderson <31716308+Dugmaren@users.noreply.github.com>
2018-02-14 14:11:46 -0800

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+// process.env.NODE_ENV is autoset to 'production'
+// by the packages
+
+if(process.env.NODE_ENV === 'production') {
+    // we're in production, return prod keys
+    module.exports = require('./prod');
+} else {
+    // we're in dev; use dev.js
+    // this is 2 lines concatenated into 1.  Require includes in our
+    // dev.js file, and setting it to the variable module.exports,
+    // sends it out.
+    module.exports = require('./dev');
+}
